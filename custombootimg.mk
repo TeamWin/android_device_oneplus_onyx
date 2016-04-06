4380d820f4da58f01798897df815b2501f7dabba
Christopher R. Palmer <crpalmer@gmail.com>
2016-04-06 13:16:25 +0100

    onyx: Compress recovery ramdisk using lzma
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,16 +1,13 @@
+LZMA_RAMDISK := $(PRODUCT_OUT)/ramdisk-recovery-lzma.img
 
+$(LZMA_RAMDISK): $(recovery_ramdisk)
+	gunzip -f < $(recovery_ramdisk) | lzma > $@
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(MINIGZIP) \
-		$(recovery_uncompressed_ramdisk) \
-		$(recovery_kernel)
-	@echo ----- Compressing recovery ramdisk ------
-	sed -i 's/ro.product.model=OnePlus X//' $(TARGET_RECOVERY_ROOT_OUT)/default.prop
-	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) | $(MINIGZIP) > $(recovery_ramdisk)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(LZMA_RAMDISK) $(recovery_kernel)
 	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(LZMA_RAMDISK)
 	@echo ----- Made recovery image -------- $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
